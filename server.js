88cf68a494f1c67391cd85c57c7684ade8b3281a
pawarsatish8292@gmail.com <satish.pawar@thefitway.io>
2023-08-25 23:38:44 +0530

    changes

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 import express from "express";
+import mysql from "mysql";
 const app = express();
+
+const db = mysql.createConnection({
+    host:"mydb.ckutuo8p605w.ap-south-1.rds.amazonaws.com",
+    port:"3306",
+    user:"admin",
+    password:"FitWay_123",
+    database:"sampledb"
+})
+db.connect((err)=>{
+    if(err){
+        console.log("err ===== ", err);
+    } else {
+        console.log("data base connected");
+    }
+})
 
 app.get('/get',(re1, res)=>{
 res.send("hello word");
